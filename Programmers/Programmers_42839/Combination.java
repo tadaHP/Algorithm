1692dd2f2a15a9c3a4a5eee6908616cf82bd9fd2
tadaHP <gusvy0220@gmail.com>
2022-07-04 03:08:25 +0900

    rename combination, change Error permutation_swap

--- Programmers/Programmers_42839/Combination.java
+++ Programmers/Programmers_42839/Combination.java
@@ -1,21 +1,21 @@
 package Programmers.Programmers_42839;
 
 public class Combination {
-    static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
-        if (r == 0) {
-            print(arr, visited, n);
+    static void combination(int[] arr, boolean[] visited, int start, int size, int  length) {
+        if (length == 0) {
+            print(arr, visited, size);
             return;
         }
-
-        for (int i = start; i < n; i++) {
+        for (int i = start; i < size; i++) {
             visited[i] = true;
-            combination(arr, visited, i + 1, n, r - 1);
+            combination(arr, visited, i + 1, size, length - 1);
             visited[i] = false;
+            
         }
     }
 
-    static void print(int[] arr, boolean[] visited, int n) {
-        for (int i = 0; i < n; i++) {
+    static void print(int[] arr, boolean[] visited, int size) {
+        for (int i = 0; i < size; i++) {
             if (visited[i]) {
                 System.out.print(arr[i] + " ");
             }
--- Programmers/Programmers_42839/Permutation_swap.java
+++ Programmers/Programmers_42839/Permutation_swap.java
@@ -1,21 +1,23 @@
 package Programmers.Programmers_42839;
 public class Permutation_swap {
-    static void perm(int[] arr, int[] output, boolean[] visited, int depth, int n, int r) {
+    static void permutation(int[] arr, int depth, int n, int r) {
         if (depth == r) {
-            print(output, r);
+            print(arr, r);
             return;
         }
      
-        for (int i=0; i<n; i++) {
-            if (visited[i] != true) {
-                visited[i] = true;
-                output[depth] = arr[i];
-                perm(arr, output, visited, depth + 1, n, r);
-                visited[i] = false;;
-            }
+        for (int i=depth; i<n; i++) {
+            swap(arr, depth, i);
+            permutation(arr, depth + 1, n, r);
+            swap(arr, depth, i);
         }
     }
-
+     
+    static void swap(int[] arr, int depth, int i) {
+        int temp = arr[depth];
+        arr[depth] = arr[i];
+        arr[i] = temp;
+    }
     static void print(int[] arr, int r) {
         for (int i = 0; i < r; i++)
             System.out.print(arr[i] + " ");
