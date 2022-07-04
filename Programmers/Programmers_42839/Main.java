de18b66b39b9768f0e4e95780307b5237f8bd5e4
tadaHP <gusvy0220@gmail.com>
2022-07-04 22:29:50 +0900

    after first run, runtime error occured

--- Programmers/Programmers_42839/Main.java
+++ Programmers/Programmers_42839/Main.java
@@ -5,6 +5,9 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException{
@@ -15,32 +18,84 @@ public class Main {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder stringBuilder = new StringBuilder();
 
-        int[] num = new int[input.length()];
-        boolean[] visited = new boolean[input.length()];
+        int size = input.length();
+        int[] numArray = new int[size];
+        ArrayList<Integer> permutationArrayList = new ArrayList<>();
+        HashSet<Integer> filterDuplication = new HashSet<Integer>();
+
         
         int answer=0;
 
-        for(int i = 0 ; i < input.length() ; i++)  num[i] = Character.getNumericValue(input.charAt(i));
+        for(int i = 0 ; i < size ; i++)  numArray[i] = Character.getNumericValue(input.charAt(i));
+
+        for(int i = 0 ; i < size +1 ; i++)
+            permutation(numArray,size,0,i, permutationArrayList);
+        
+        Collections.sort(permutationArrayList);
+        
+        boolean[] isPrime = eratosthenes(permutationArrayList.get(permutationArrayList.size()-1)+1);
+        
+        
         
 
-    
-        // boolean[] prime = new boolean[biggestNum];
-        // prime[0] = true; prime[1]=true;
-
-        // for(int i = 2 ; i < biggestNum ; i++){
-        //     if(!prime[i]){
-        //         for(int j = i* i ; j < biggestNum+1 ; j += i){
-        //             prime[j] = true;
-        //         }
-        //     }
-        // }
-
+        for(int i =0 ; i<permutationArrayList.size() ; i++){
+            if(!isPrime[permutationArrayList.get(i)]) {
+                filterDuplication.add(permutationArrayList.get(i));
+                System.out.println(permutationArrayList.get(i));
+            }
+        }
+        
+        for(int i : filterDuplication) answer++;
+        
         
         stringBuilder.append(answer);
-        bufferedWriter.write(stringBuilder.toString());
+        bufferedWriter.write("answer: " + stringBuilder.toString());
 
         bufferedWriter.close();
 
+    }
+
+    public static void permutation(int[] arr, int size, int start, int length, ArrayList<Integer> arrayList){
+        if(start == length){
+            StringBuilder tempBuilder = new StringBuilder();
+            for (int i = 0; i < length; i++)  tempBuilder.append(arr[i]);
+            
+            try {
+                arrayList.add(Integer.parseInt(tempBuilder.toString()));
+            } catch (Exception e) {
+                
+            }
+            tempBuilder.setLength(0);
+        }
+        else{
+            for(int i = start ; i < size ; i ++){
+                swap(arr, start, i);
+                permutation(arr, size, start+1, length, arrayList);
+                swap(arr, start, i);
+            }
+        }
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static boolean[] eratosthenes(int size){
+
+        boolean[] prime = new boolean[size];
+        prime[0] = true; prime[1]=true;
+
+        for(int i = 2 ; i < size ; i++){
+            if(!prime[i]){
+                //prime[i] = false
+                for(int j = i* i ; j < size ; j += i){
+                    prime[j] = true;
+                }
+            }
+        }
+        return prime;
     }
 
 
