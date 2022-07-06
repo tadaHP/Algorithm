ac304e7eef5219ccd05152983f12c10b0077bfcb
tadaHP <gusvy0220@gmail.com>
2022-07-06 23:34:17 +0900

    Programmers_42862 - second try

--- Programmers/Programmers_42862/Main.java
+++ Programmers/Programmers_42862/Main.java
@@ -1,23 +1,18 @@
 package Programmers.Programmers_42862;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        int n = 5;
         
-        // int n = 5;
-        // int[] lost = {2, 4};
-        // int[] reserve ={1,3,5};
-
-        // int[] lost = {2, 4};
-        // int[] reserve ={3};
-
-        int n = 3;
-        int[] lost = {3};
-        int[] reserve ={1};
+        int[] lost = {2, 4};
+        int[] reserve ={3, 1};
 
         boolean[] isBorrow = new boolean[n];
         int answer=n-lost.length;
+
+        Arrays.sort(reserve);
 
         for(int i =0; i<lost.length ;i++){
             for(int j: reserve){
@@ -26,13 +21,18 @@ public class Main {
                     isBorrow[lost[i]-2] = true;
                     break;
                 }
+                if(Integer.compare(lost[i], j)==0 && !isBorrow[lost[i]-1]){
+                    answer++;
+                    isBorrow[lost[i]-1] = true;
+                    break;
+                }
                 if(Integer.compare(lost[i]+1, j)==0 && !isBorrow[lost[i]]){ //lost+1 == reserve
                     answer++;
                     isBorrow[lost[i]] = true;
+                    break;
                 }
             }
         }
         System.out.println("answer: " + answer);
-
     }
 }
