0b9cecd7f61f2d8e9acd458d621df9eb943bb291
tadaHP <gusvy0220@gmail.com>
2023-11-22 14:12:04 +0900

    Baekjoon_1297 수정

--- Baekjoon/Baekjoon_1297/Main.java
+++ Baekjoon/Baekjoon_1297/Main.java
@@ -6,21 +6,16 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		String[] split = br.readLine().split(" ");
 		int d = Integer.parseInt(split[0]);
 		int h = Integer.parseInt(split[1]);
 		int w = Integer.parseInt(split[2]);
 
-		sb.append(solve(h, w, d, h)).append(" ").append(solve(h, w, d, w));
-		System.out.println(sb);
-
+		System.out.println(solve(h, w, d));
 	}
 
-	public static int solve(int x, int y, int d, int target) {
-
-		double v = (d * target) / Math.sqrt(x * x + y * y);
-
-		return (int)v;
+	public static String solve(double h, double w, double d) {
+		double sqrt = Math.sqrt((d * d) / ((h * h) + (w * w)));
+		return (int)(sqrt * h) + " " + (int)(sqrt * w);
 	}
 }
