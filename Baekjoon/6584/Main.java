62675bdafa21362da7f0cb354dd6fa27ad1ff3b0
hyeonpyo <gusvy0220@gmail.com>
2025-06-18 23:41:06 +0900

    OOP 스타일로 변경

--- Baekjoon/6584/Main.java
+++ Baekjoon/6584/Main.java
@@ -1,114 +1,104 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         while (true) {
-            int[] init = Arrays.stream(reader.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+            String line = reader.readLine();
+            if (line == null || line.isEmpty()) continue;
 
-            if(init[0] == 0 && init[1] == 0) {
-                break;
+            String[] init = line.split(" ");
+            int numberOfNode = Integer.parseInt(init[0]);
+            int numberOfEdge = Integer.parseInt(init[1]);
+
+            if (numberOfNode == 0 && numberOfEdge == 0) break;
+
+            Map<Integer, Node> graph = new HashMap<>();
+            for (int i = 0; i < numberOfNode; i++) {
+                graph.put(i, new Node(i));
             }
 
-            int numberOfNode = init[0];
-            int numberOfEdge = init[1];
-            int answer = -1;
+            for (int i = 0; i < numberOfEdge; i++) {
+                String[] edgeInfo = reader.readLine().split(" ");
+                int a = Integer.parseInt(edgeInfo[0]);
+                int b = Integer.parseInt(edgeInfo[1]);
 
-            // 그래프를 객체? 기준으로 만들어야 할것으로 보임 가능한지는 미지수
-            // LinkedList 형식으로 객체를 만들고, 방문하면서 진행, 무방문시에는 방문처리
+                Node nodeA = graph.get(a);
+                Node nodeB = graph.get(b);
+                Edge edge = new Edge(nodeA, nodeB);
 
-            HashMap<Integer, Node> graphMap = new HashMap<>();
-            for(int i = 0; i < numberOfNode; i++) {
-                graphMap.put(i, new Node(i));
+                nodeA.addEdge(edge);
+                nodeB.addEdge(edge);
             }
 
-            // numberOfEdge 만큼의 간선 정보를 입력해야함
-            for(int i = 0 ; i < numberOfEdge; i++) {
-                int[] edgeInfo = Arrays.stream(reader.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-                int a = edgeInfo[0];
-                int b = edgeInfo[1];
-                Node nodeA = graphMap.get(a);
-                Node nodeB = graphMap.get(b);
-                Main.Edge edge = new Edge(List.of(nodeA, nodeB));
-                nodeA.getEdges().add(edge);
-                nodeB.getEdges().add(edge);
+            int longest = 0;
+            for (Node node : graph.values()) {
+                longest = Math.max(longest, node.findLongestPath());
             }
 
-            for(Node node : graphMap.values()) {
-                int dfs = dfs(node);
-                answer = Math.max(answer, dfs);
-            }
-            
-            writer.write(String.valueOf(answer - 1));
+            writer.write(String.valueOf(longest - 1));
             writer.newLine();
         }
-        
+
         writer.flush();
-
-        reader.close();
         writer.close();
+        reader.close();
     }
 
-    public static int dfs(Node node) {
-        int max = 0;
-
-        for (Edge edge : node.getEdges()) {
-            if (!edge.used) {
-                edge.used = true;
-                for(Node nextNode : edge.getTo()){
-                    if (nextNode != node) {
-                        max = Math.max(max, dfs(nextNode));
-                    }
-                }
-                edge.used = false;
-            }
-        }
-
-        return max + 1;
-    }
-
-    public static class Node{
-        private int number;
-        List<Edge> edges;
+    static class Node {
+        private final int number;
+        private final List<Edge> edges = new ArrayList<>();
 
         public Node(int number) {
             this.number = number;
-            this.edges = new ArrayList<>();
         }
 
-        public int getNumber() {
-            return number;
+        public void addEdge(Edge edge) {
+            edges.add(edge);
         }
-        public List<Edge> getEdges() {
-            return edges;
+
+        public int findLongestPath() {
+            int max = 0;
+            for (Edge edge : edges) {
+                if (!edge.isUsed()) {
+                    edge.markUsed();
+                    Node next = edge.getOtherNode(this);
+                    max = Math.max(max, next.findLongestPath());
+                    edge.unMarkUsed();
+                }
+            }
+            return max + 1;
         }
     }
 
-    public static class Edge {
-        List<Node> to;
-        boolean used;
+    static class Edge {
+        private final Node nodeA;
+        private final Node nodeB;
+        private boolean used;
 
-        public Edge(List<Node> to) {
-            this.to = to;
+        public Edge(Node a, Node b) {
+            this.nodeA = a;
+            this.nodeB = b;
             this.used = false;
         }
 
-        public List<Node> getTo() {
-            return to;
+        public Node getOtherNode(Node from) {
+            return from == nodeA ? nodeB : nodeA;
         }
+
         public boolean isUsed() {
             return used;
         }
-        public void setUsed(boolean used) {
-            this.used = used;
+
+        public void markUsed() {
+            used = true;
+        }
+
+        public void unMarkUsed() {
+            used = false;
         }
     }
 }
